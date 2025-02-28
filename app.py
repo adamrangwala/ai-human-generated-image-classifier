68adc9fdf69699e8ecd831c742013bf5e1d6ce48
adamrangwala <35049289+adamrangwala@users.noreply.github.com>
2025-02-28 13:29:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def load_model():
         return None
 
 # Function to preprocess the image
-def preprocess_image(image, target_size=(224, 224)):
+def preprocess_image(image, target_size=(780, 780)):
     """Preprocess the image to be compatible with the model"""
     # Resize image
     image = image.resize(target_size)
